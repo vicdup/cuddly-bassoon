168145ef1428c323690c1986bbccdf244180af3e
seitrec <corentin_seitre@hotmail.fr>
2015-12-10 16:09:13 +0100

    new|crawler.py: testing tmdbsimple

---
+++ crawler.py
@@ -0,0 +1,11 @@
+import tmdbsimple as tmdb
+import sys
+
+tmdb.API_KEY = '1a3f1b0a8620851f42d4b1a95494d44d'
+
+if __name__ == "__main__":
+    search = tmdb.Search()
+    query = raw_input("Please enter something: ")
+    response = search.movie(query=query)
+    for s in search.results:
+        print(s['title'], s['id'], s['release_date'], s['popularity'])
