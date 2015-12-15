5fc42672fef15d9713f554dc44ab8263ea6f5cbb
AuroreM <aurore.malherbes@student.ecp.fr>
2015-12-15 09:56:15 +0100

    1ere utilisation de discover

--- basicSearch.py
+++ basicSearch.py
@@ -3,7 +3,19 @@ import sys
 
 tmdb.API_KEY = '1a3f1b0a8620851f42d4b1a95494d44d'
 
+"""
 movie = tmdb.Movies(603)
 response = movie.info()
 print(movie.title)
 print(movie.budget)
+
+search = tmdb.Search()
+response = search.movie(query = "suits")
+for s in search.results:
+	print(s['title'], s['id'], s['release_date'], s['popularity'])
+"""
+
+discover = tmdb.Discover()
+response = discover.movie(year=2015, sort_by = "vote_average.asc")
+for r in discover.results:
+	print(r['title'], r['vote_average'], r['release_date'], r['popularity'])
