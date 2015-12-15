f956e09afca4e9da33d0d2fdddec6f4b8990a116
AuroreM <aurore.malherbes@student.ecp.fr>
2015-12-15 10:11:36 +0100

    Recherche par popularité

--- basicSearch.py
+++ basicSearch.py
@@ -1,5 +1,6 @@
 import tmdbsimple as tmdb
 import sys
+import json
 
 tmdb.API_KEY = '1a3f1b0a8620851f42d4b1a95494d44d'
 
@@ -16,6 +17,16 @@ for s in search.results:
 """
 
 discover = tmdb.Discover()
-response = discover.movie(year=2015, sort_by = "vote_average.asc")
-for r in discover.results:
-	print(r['title'], r['vote_average'], r['release_date'], r['popularity'])
+responseByPop = discover.movie(sort_by = 'popularity.desc')
+responseByPop = json.dumps(responseByPop)
+print(responseByPop)
+#for r in discover.results:
+#	print(r['title'], r['popularity'])
+
+"""
+#La query est toujours obligatoire et se fait sur le titre
+search = tmdb.Search()
+response = search.movie(query = "action", year = 2015)
+for s in search.results:
+	print(s['title'], s['id'], s['release_date'], s['popularity'])
+"""
