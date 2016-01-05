697c71c1c578f0482719ee69eae557588cdfa339
AuroreM <aurore.malherbes@student.ecp.fr>
2016-01-05 12:49:32 +0100

    Input des données dans serie.html

--- angular-phonecat/app/js/services.js
+++ angular-phonecat/app/js/services.js
@@ -6,13 +6,22 @@ var cuddlyServices = angular.module('cuddlyServices', ['ngResource']);
 
 cuddlyServices.service('apiTmdb', function($http){
 	var apiTmdb = {
-		getSerieById: function(idTv) {
-			var promise = $http.get('https://api.themoviedb.org/3/tv/'+idTv+'?api_key=1a3f1b0a8620851f42d4b1a95494d44d').then(function (response){
+		getSerieById: function(idSerie) {
+			var promise = $http.get('https://api.themoviedb.org/3/tv/'+idSerie+'?api_key=1a3f1b0a8620851f42d4b1a95494d44d').then(function (response){
+				console.log(response);
+				return response.data;
+			})
+			return promise;
+		},
+		getSeasonByNumberSeason: function(idSeason,idSerie) {
+			var promise = $http.get('https://api.themoviedb.org/3/tv/'+idSerie+'/season/'+idSeason+'?api_key=1a3f1b0a8620851f42d4b1a95494d44d').then(function (response){
 				console.log(response);
 				return response.data;
 			})
 			return promise;
 		}
+
+
 	};
 	return apiTmdb;
 
