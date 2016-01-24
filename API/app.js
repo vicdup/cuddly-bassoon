e92b5bd172f62f5c47aebf9d5ca8d97eb07a054e
unknown <vicdup@gmail.com>
2016-01-24 17:45:56 +0100

    changed allowed origins to * (unsafe)

--- API/app.js
+++ API/app.js
@@ -9,7 +9,7 @@ var app = express();
 mongoose.connect('mongodb://localhost',function(err){
   if (err) throw err
   app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
